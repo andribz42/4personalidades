7856e5ef830eec5fd681ed9b32a21c4c67a02aac
andribz42 <andersonpribeiro7@gmail.com>
2023-05-18 11:00:36 -0300

    adicionando personalidade

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"estudo/4personalidades-go/routes"
+	"go-rest-api/models"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "História 1"},
+		{Nome: "Nome 2", Historia: "História 2"},
+	}
 	routes.HandleRequest()
 }
